815f0e662a5307c2ee28d3ffc72de9e66bbbd73f
Antonio SJ Musumeci <trapexit@spawn.link>
2025-01-22 11:58:34 -0600

    cel_rotation.cpp

--- src/cel_rotation.cpp
+++ src/cel_rotation.cpp
@@ -106,7 +106,8 @@ main_cel_rotation()
       display.clear();
       display.draw_cels(logo);
       display.sc->sc_Bitmap[display.screen()]->bm_Buffer += 320;
-      display.draw_cels(logo);      
+      display.draw_cels(logo);
+      display.sc->sc_Bitmap[display.screen()]->bm_Buffer -= 320;      
       display.draw_printf(16,16,"x: %d",ConvertF16_32(x));
       display.draw_printf(16,24,"y: %d",ConvertF16_32(y));
       //      display.display_and_swap();
