065874ae42bc1bdf1336cb1d8ee60ac47f7cacd4
Antonio SJ Musumeci <trapexit@spawn.link>
2025-01-22 12:11:37 -0600

    cel_rotation.cpp

--- src/cel_rotation.cpp
+++ src/cel_rotation.cpp
@@ -104,14 +104,14 @@ main_cel_rotation()
       //             ConvertF16_32(y));
       //   }
 
-      SetClipOrigin(display.sc->sc_BitmapItems[display.screen()],0,0);
-      display.draw_cels(logo);
+      //      SetClipOrigin(display.sc->sc_BitmapItems[display.screen()],0,0);
+      //      display.draw_cels(logo);
       //      display.sc->sc_Bitmaps[display.screen()]->bm_Buffer +=
       //      320;
       SetClipOrigin(display.sc->sc_BitmapItems[display.screen()],160,0);
       logo->ccb_XPos += 1 << 16;
       display.draw_cels(logo);
-      logo->ccb_XPos -= 1 << 16;      
+      //      logo->ccb_XPos -= 1 << 16;      
       //      display.sc->sc_Bitmaps[display.screen()]->bm_Buffer -= 320;      
       display.draw_printf(16,16,"x: %d",ConvertF16_32(x));
       display.draw_printf(16,24,"y: %d",ConvertF16_32(y));
