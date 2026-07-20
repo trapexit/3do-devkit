15dcaedb247e820f59035a6d8583f602f6d2e4a7
Antonio SJ Musumeci <trapexit@spawn.link>
2026-07-20 15:07:32 -0500

    Allow bootstrap to preserve existing takeme directories

--- bin/buildtools/win/bootstrap-3do-project.bat
+++ bin/buildtools/win/bootstrap-3do-project.bat
@@ -13,21 +13,18 @@ if /I "%DST_PATH%"=="%SRC_PATH%" (
     exit /b 1
 )
 
-rem Check if the 'takeme' folder already exists in the target path
 if exist "%DST_PATH%\takeme\" (
-    echo Error: 'takeme' folder already exists in the target path. Aborting.
-    if [%arg0:~2,1%]==[:] pause
-    exit /b 1
-)
-
-echo "Copying takeme (core CDROM filesystem contents)"
-mkdir "%DST_PATH%\takeme"
-copy /Y "%SRC_PATH%\takeme\AppStartup" "%DST_PATH%\takeme\AppStartup"
-copy /Y "%SRC_PATH%\takeme\BannerScreen" "%DST_PATH%\takeme\BannerScreen"
-xcopy "%SRC_PATH%\takeme\System" "%DST_PATH%\takeme\System" /Q /E /I /C /-Y
-IF EXIST "%DST_PATH%\data" (
-   echo "Copying data/* into takeme/"
-   xcopy "%DST_PATH%\data\*" "%DST_PATH%\takeme" /Q /E /I /C /-Y
+    echo "'takeme' directory exists. Skipping."
+) ELSE (
+    echo "Copying takeme (core CDROM filesystem contents)"
+    mkdir "%DST_PATH%\takeme"
+    copy /Y "%SRC_PATH%\takeme\AppStartup" "%DST_PATH%\takeme\AppStartup"
+    copy /Y "%SRC_PATH%\takeme\BannerScreen" "%DST_PATH%\takeme\BannerScreen"
+    xcopy "%SRC_PATH%\takeme\System" "%DST_PATH%\takeme\System" /Q /E /I /C /-Y
+    IF EXIST "%DST_PATH%\data" (
+       echo "Copying data/* into takeme/"
+       xcopy "%DST_PATH%\data\*" "%DST_PATH%\takeme" /Q /E /I /C /-Y
+    )
 )
 IF NOT EXIST "%DST_PATH%\src" (
    echo "Copying starter src/main.c"
