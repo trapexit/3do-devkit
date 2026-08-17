435e2f972d99453f91195024ef92533aad0a007e
Antonio SJ Musumeci <trapexit@spawn.link>
2026-08-17 15:22:59 -0500

    Add assert.h and STATIC_ASSERT macro

---
+++ include/3do/assert.h
@@ -0,0 +1,25 @@
+#pragma force_top_level
+#pragma include_only_once
+
+/* Runtime and compile-time assertion interfaces. */
+
+#ifndef STATIC_ASSERT
+#define STATIC_ASSERT(cond, msg) \
+  typedef char static_assertion_##msg[(cond) ? 1 : -1]
+#endif
+
+#ifdef __cplusplus
+extern "C" void __assert(const char *, const char *, int);
+#else
+extern void __assert(const char *, const char *, int);
+#endif
+
+#ifdef NDEBUG
+#define assert(ignore) ((void)0)
+#else
+#ifdef __STDC__
+#define assert(e) ((e) ? (void)0 : __assert(#e, __FILE__, __LINE__))
+#else
+#define assert(e) ((e) ? (void)0 : __assert("e", __FILE__, __LINE__))
+#endif
+#endif
